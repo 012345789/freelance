7878c9f506735c8c27b2d6656788205d7bd194a1
Gorlan Yuen <holyduper@gmail.com>
2015-11-19 23:24:33 -0800

    Add script to start server in package.json.

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
 	var host = server.address().address;
     var port = server.address().port;
 
